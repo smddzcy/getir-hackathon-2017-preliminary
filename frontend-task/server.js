a0e88c6c3c00078cbf7d585c8de9d5b92dba8df4
Samed Duzcay <samedduzcay@gmail.com>
2017-03-03 17:07:05 +0300

    Render index on /

--- frontend-task/server.js
+++ frontend-task/server.js
@@ -5,4 +5,9 @@ const express = require('express');
 const app = express();
 
 app.use(gzippo.staticGzip(__dirname + "/"));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
 app.listen(process.env.PORT || 5000);
