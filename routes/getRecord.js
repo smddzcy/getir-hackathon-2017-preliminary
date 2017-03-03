f97db6e36bb1757021ff87ef98c698b257da17eb
Samed Duzcay <samedduzcay@gmail.com>
2017-03-03 15:13:23 +0300

    Finish the implementation of getRecord endpoint.

--- routes/getRecord.js
+++ routes/getRecord.js
@@ -1,23 +1,54 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/*
-  POST getRecord.
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
-  Accepts a JSON object in the format:
-  {
-    "key": "chp8vgSkJDbyDKAS"
-  }
-
-  Returns a JSON object in the format:
-  {
-    "key" : "chp8vgSkJDbyDKAS",
-    "value" : "9sScg6DaMLbMTgLdPADPYJTyOd6AgHDs9SZNkgGTlHeQF",
-    "createdAt" : "2016-01-26"
-  }
+/**
+ * Finds a record from the Getir Hackathon database with the given key.
+ * @param key Key of the record.
+ * @returns {Promise} Resolves with the collection of record
  */
-router.post('/getRecord', function(req, res, next) {
-  res.send('respond with a meaningful object.');
+const findRecord = (key) => {
+  return new Promise((resolve, reject) => {
+    db.open().then((db) => {
+      const records = db.collection('records');
+
+      // Find the document with the key and resolve the promise.
+      records.findOne({key: key})
+        .then(resolve)
+        .catch(reject);
+    }).catch(reject);
+  });
+};
+
+/**
+ * POST /getRecord
+ *
+ * Accepts a JSON object in the format:
+ * {
+ *   "key": "chp8vgSkJDbyDKAS"
+ * }
+ *
+ * Returns a JSON object in the format:
+ * {
+ *   "key" : "chp8vgSkJDbyDKAS",
+ *   "value" : "9sScg6DaMLbMTgLdPADPYJTyOd6AgHDs9SZNkgGTlHeQF",
+ *   "createdAt" : "2016-01-26"
+ * }
+ */
+router.post('/getRecord', (req, res, next) => {
+  const key = req.body.key;
+  if (!key) {
+    // Return an error, request body doesn't contain a "key" field.
+    const err = new Error('Request body should contain a "key" field.');
+    err.status = 422;
+    next();
+  }
+
+  findRecord(key).then((record) => {
+    res.json(record);
+  }).catch(() => next());
 });
 
 module.exports = router;
