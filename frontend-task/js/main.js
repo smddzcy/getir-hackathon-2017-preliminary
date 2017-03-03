d4d34f2afb97c273e386764510428e102f09cd0d
Samed Duzcay <samedduzcay@gmail.com>
2017-03-03 16:45:29 +0300

    Implement the date picker and http request.

--- frontend-task/js/main.js
+++ frontend-task/js/main.js
@@ -1,7 +1,24 @@
 'use strict';
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', [
+  '720kb.datepicker'
+]);
 
-app.controller('MainContoller', ['$http', function ($http) {
-  this.w = 123;
+app.controller('MainContoller', ['$http', '$scope', function ($http, $scope) {
+  this.send = () => {
+    const endpoint = "https://getir-bitaksi-hackathon.herokuapp.com/getRecords";
+
+    $http.post(endpoint, {
+      startDate: this.startDate,
+      endDate: this.endDate
+    }).then((res) => {
+      this.records = res.records;
+      // TODO: Check records
+    }).catch((err) => {
+      console.log("Error:", err);
+    });
+  };
+
+  // TODO: Error checking
+  $scope.$watch(() => this.date, (v, old) => console.log(v));
 }]);
